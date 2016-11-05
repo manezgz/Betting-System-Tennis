94539ea76413ecd147361a49cdd24142549671c7
Jose Manuel Franco <manezgz@hotmail.com>
2016-11-05 20:02:15 +0100

    Change sh file

--- AtpScrapy/spyder_launch.sh
+++ AtpScrapy/spyder_launch.sh
@@ -1,18 +1,19 @@
-rm /home/tournaments_2016.csv
-scrapy runspider atp_spyder.py -a year=2000 -o tournaments_2000.csv
-scrapy runspider atp_spyder.py -a year=2001 -o tournaments_2001.csv
-scrapy runspider atp_spyder.py -a year=2002 -o tournaments_2002.csv
-scrapy runspider atp_spyder.py -a year=2003 -o tournaments_2003.csv
-scrapy runspider atp_spyder.py -a year=2004 -o tournaments_2004.csv
-scrapy runspider atp_spyder.py -a year=2005 -o tournaments_2005.csv
-scrapy runspider atp_spyder.py -a year=2006 -o tournaments_2006.csv
-scrapy runspider atp_spyder.py -a year=2007 -o tournaments_2007.csv
-scrapy runspider atp_spyder.py -a year=2008 -o tournaments_2008.csv
-scrapy runspider atp_spyder.py -a year=2009 -o tournaments_2009.csv
-scrapy runspider atp_spyder.py -a year=2010 -o tournaments_2010.csv
-scrapy runspider atp_spyder.py -a year=2011 -o tournaments_2011.csv
-scrapy runspider atp_spyder.py -a year=2012 -o tournaments_2012.csv
-scrapy runspider atp_spyder.py -a year=2013 -o tournaments_2013.csv
-scrapy runspider atp_spyder.py -a year=2014 -o tournaments_2014.csv
-scrapy runspider atp_spyder.py -a year=2015 -o tournaments_2015.csv
-scrapy runspider atp_spyder.py -a year=2016 -o tournaments_2016.csv
+rm /home/Betting-System-Tennis/AtpScrapy/data/atp/*.csv
+
+#scrapy runspider atp_spyder.py -a year=2000 -o data/tournaments_2000.csv
+#scrapy runspider atp_spyder.py -a year=2001 -o data/tournaments_2001.csv
+#scrapy runspider atp_spyder.py -a year=2002 -o data/tournaments_2002.csv
+#scrapy runspider atp_spyder.py -a year=2003 -o data/tournaments_2003.csv
+#scrapy runspider atp_spyder.py -a year=2004 -o data/tournaments_2004.csv
+#scrapy runspider atp_spyder.py -a year=2005 -o data/tournaments_2005.csv
+#scrapy runspider atp_spyder.py -a year=2006 -o data/tournaments_2006.csv
+#scrapy runspider atp_spyder.py -a year=2007 -o data/tournaments_2007.csv
+#scrapy runspider atp_spyder.py -a year=2008 -o data/tournaments_2008.csv
+#scrapy runspider atp_spyder.py -a year=2009 -o data/tournaments_2009.csv
+#scrapy runspider atp_spyder.py -a year=2010 -o data/tournaments_2010.csv
+#scrapy runspider atp_spyder.py -a year=2011 -o data/tournaments_2011.csv
+#scrapy runspider atp_spyder.py -a year=2012 -o data/tournaments_2012.csv
+#scrapy runspider atp_spyder.py -a year=2013 -o data/tournaments_2013.csv
+#scrapy runspider atp_spyder.py -a year=2014 -o data/tournaments_2014.csv
+#scrapy runspider atp_spyder.py -a year=2015 -o data/tournaments_2015.csv
+scrapy runspider atp_spyder.py -a year=2016 -o data/tournaments_2016.csv
